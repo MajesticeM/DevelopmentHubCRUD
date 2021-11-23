cf3d64c564d3e8cad5e72a0d2455217f60d98967
TanzoTheGreatest <ralephatathanyani@gmail.com>
2021-11-23 13:17:40 +0200

    ArrayCrud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DevelopmentHubCRUD';
+ myArray=[1,2,4,5,7,8,9,0];
+ UpdateName: any;
+ Create()
+ {
+   this.myArray.push(1);
+   console.log(this.myArray);
+ }
+Read(SearchItem: number)
+{
+for (let index = 0; index < this.myArray.length; index++) {
+  const element = this.myArray[index];
+  if (SearchItem===element)
+  {
+    return element;
+  }
 }
+Update(UpdateName,index: number)
+{
+
+}
+
+Delete(ArrayItemNumber)
+{
+  this.myArray.splice(ArrayItemNumber,1);
+  console.log(this.myArray);
+}
+}
+}
+
+
